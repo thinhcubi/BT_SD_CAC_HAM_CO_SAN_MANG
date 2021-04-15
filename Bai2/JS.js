25bbfa496ee3ae62370e5dbf62823984aa50e908
thine <thinhcubi27@gmail.com>
2021-04-15 14:18:40 +0700

    Bai2

---
+++ Bai2/JS.js
@@ -0,0 +1,12 @@
+let numbers=window.prompt();
+let str=numbers.toString();
+let result = [str[0]];
+
+for (let x=1;x<str.length; x++) {
+   if (str[x -1 ] %2===0 && str[x]%2 ===0) {
+       result.push('-',str[x]);
+   } else {
+       result.push(str[x]);
+   }
+}
+console.log(result.join(''));
